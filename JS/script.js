4ace480c6dc63ac7a0328571eac261a00ec38eaf
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-10-29 00:10:01 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,78 @@
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const allLinks = document.querySelectorAll('a:link');
+const headerEl = document.querySelector('.navigation-header');
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    const href = link.getAttribute('href');
+
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+      e.preventDefault();
+    }
+
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
+// Sticky Navigation
+const navigationHeader = document.querySelector('.navigation-header');
+const initialNavigation = document.querySelector('.initial-navigation');
+const stickyNavigation = document.querySelector('.sticky-nav');
+const sectionHero = document.querySelector('.section-hero');
+
+document.addEventListener('DOMContentLoaded', function () {
+  let lastScrollTop = 0;
+
+  function handleScroll() {
+    const scrollTop = window.scrollY;
+
+    if (scrollTop > 40 && scrollTop > lastScrollTop) {
+      navigationHeader.classList.add('sticky-nav');
+      navigationHeader.classList.remove('initial-navigation');
+    } else if (scrollTop <= 40 && scrollTop < lastScrollTop) {
+      navigationHeader.classList.remove('sticky-nav');
+      navigationHeader.classList.add('initial-navigation');
+    }
+
+    lastScrollTop = scrollTop;
+  }
+
+  window.addEventListener('scroll', handleScroll);
+});
+
+// JavaScript for the slideshow transitions
+const images = document.querySelectorAll('.slideshow-image');
+let currentIndex = 0;
+
+if (images.length > 0) {
+  console.log(images.length);
+  function showNextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length; // Cycle through images
+    images[currentIndex].classList.add('active');
+  }
+
+  // Change image every 6 seconds
+  setInterval(showNextImage, 6000);
+}
+
+// Project Swiper images
+document.querySelectorAll('.project-slide_overlay').forEach((el) => {
+  const bg = el.getAttribute('data-bg');
+  if (bg) {
+    el.style.backgroundImage = `url(${bg})`;
+  }
+});
---
+++ JS/swiper.js
@@ -0,0 +1,90 @@
+var swiper = new Swiper('.swiper-3', {
+  slidesPerView: 'auto',
+  spaceBetween: 15,
+  freeMode: true,
+  centeredSlides: true,
+  //   loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  grid: {
+    rows: 2,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper('.mySwiper-2', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var swiper = new Swiper('.swiper-3', {
+  slidesPerView: 'auto',
+  spaceBetween: 15,
+  freeMode: true,
+  centeredSlides: true,
+  //   loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper('.swiper-4', {
+  slidesPerView: 2,
+  spaceBetween: 15,
+  freeMode: true,
+  centeredSlides: false,
+  loop: true,
+  //   autoHeight: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+// Select all the elements you want to observe for animations
+const statCards = document.querySelectorAll('.feature-card');
+
+// Set up the observer options (you can tweak these if necessary)
+const observerOptions = {
+  root: null, // default to the viewport
+  threshold: 0.1, // trigger when 10% of the element is visible
+};
+
+// Define the observer callback function
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('fade-in-visible');
+      observer.unobserve(entry.target); // Optional: Stop observing once it's in view
+    }
+  });
+};
+
+// Create an observer instance
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+// Add each card to the observer with the fade-in class initially
+statCards.forEach((card) => {
+  card.classList.add('fade-in');
+  observer.observe(card);
+});
